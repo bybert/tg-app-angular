bfad15f9d0603d56c2fba3a598b4380740cefc6a
bybert <74455519+bybert@users.noreply.github.com>
2023-12-13 13:00:12 +0300

    created angular-tg-app

---
+++ src/app/pages/feedback/feedback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [],
+  template: 'feedback'
+})
+export class FeedbackComponent {
+
+}
